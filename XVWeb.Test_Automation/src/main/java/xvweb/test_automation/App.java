b1eb94a16790946a51063b1aaec11ea7091055aa
AzureAD\KaushalSathavara <ksathavara@planetdds.com>
2021-07-27 11:18:01 -0700

    First Commit

---
+++ XVWeb.Test_Automation/src/main/java/xvweb/test_automation/App.java
@@ -0,0 +1,15 @@
+package xvweb.test_automation;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+	public void test() {
+		System.out.println( "Hello World!" );
+	}
+}
